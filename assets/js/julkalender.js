1c8138e4d2eb1bb2b7dbf87a45fd186b792e9835
Mattias Måsbäck <teddykladdkak@gmail.com>
2021-11-05 13:16:59 +0100

    Julkalender
    - Större justeringar utifrån återkoppling.
    - Popup innehåller nu mer ingående information.

--- assets/js/julkalender.js
+++ assets/js/julkalender.js
@@ -6,23 +6,40 @@ Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vT90tUWUvDqVMGVvMKXG
     complete: function(results) {
         for (var a = 0; a < results.data.length; a++){
             if(results.data[a][0] == 'ID'){}else{
-                if(results.data[a][2] == 'TRUE'){
+                if(results.data[a][5] == 'TRUE'){
                     var obj = document.getElementById('julKalender' + results.data[a][0]);
                         obj.setAttribute('class', 'jul-kalender active');
-                        obj.setAttribute('onclick', 'modal("' + results.data[a][1] + '");');
+                        obj.setAttribute('onclick', 'modal("' + results.data[a][2] + '", "' + results.data[a][1] + '", "' + results.data[a][3] + '", "' + results.data[a][4] + '");');
+                        if(getParameterByName('julkalender') == results.data[a][0]){
+                            modal(results.data[a][2], results.data[a][1], results.data[a][3], results.data[a][4]);
+                        };
                 };
             };
         };
     }
 });
-function modal(text){
+function modal(text, datum, ref, href){
+    document.getElementById('kalender-datum').innerText = datum;
     document.getElementById('kalender-message').innerText = text;
+    var objRef = document.getElementById('kalender-reference');
+        removeElements(objRef);
+        if(!href || href == '' ){
+            var linkT = document.createTextNode(ref);
+            objRef.appendChild(linkT);
+        }else{
+            var link = document.createElement('a');
+                link.setAttribute('href', href);
+                link.setAttribute('target', '_blank');
+                link.setAttribute('alt', 'Länk till referens av citat');
+                var linkT = document.createTextNode(ref);
+                link.appendChild(linkT);
+            objRef.appendChild(link);
+        };
     document.getElementById("myModal").style.display = "block";        
 };
 function stangModal(){
     document.getElementById("myModal").style.display = "none";
 };
-
 window.onclick = function(event) {
     var modal = document.getElementById("myModal");
     if (event.target == modal) {
