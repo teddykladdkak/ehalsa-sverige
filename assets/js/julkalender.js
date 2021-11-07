bad17a1fc65181dfc249627233e633356a648e30
Mattias Måsbäck <teddykladdkak@gmail.com>
2021-11-07 23:04:18 +0100

    Julkalender
    - Delningsknappar för specifik lucka finns nu i popuprutan.
    - Kalender ID är nu nummret.
    - Rutor visas nu ifall datum är passerat.
    - Lade till variabel "datum" för testning.

--- assets/js/julkalender.js
+++ assets/js/julkalender.js
@@ -1,3 +1,8 @@
+/*
+# API
+* lucka: NUM (Gör att lucka med aktuellt nummer öppnas direkt vid sidladdning)
+* datum: ÅÅÅÅ-MM-DD (Forcera eget dagens datum, till tester)
+*/
 Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vT90tUWUvDqVMGVvMKXGD55BTDUa--DnjBSWoYKyUF9QCs-YNumse-c2NBjdMTL2ZNfHZt7h2kNlPyN/pub?output=csv", {
     download: true,
     error: function(results) {
@@ -6,21 +11,33 @@ Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vT90tUWUvDqVMGVvMKXG
     complete: function(results) {
         for (var a = 0; a < results.data.length; a++){
             if(results.data[a][0] == 'ID'){}else{
-                if(results.data[a][5] == 'TRUE'){
+                var luckaDatum = '2021-12-' + addZero(results.data[a][0]);
+                if(getParameterByName('datum')){
+                    var checkDate = dateInPast(luckaDatum, getParameterByName('datum'));
+                }else{
+                    var checkDate = dateInPast(luckaDatum);
+                };
+                /*if(results.data[a][5] == 'TRUE' || checkDate){*/
+                if(checkDate){
                     var obj = document.getElementById('julKalender' + results.data[a][0]);
                         obj.setAttribute('class', 'jul-kalender active');
-                        obj.setAttribute('onclick', 'julkalenderModal("' + results.data[a][2] + '", "' + results.data[a][1] + '", "' + results.data[a][3] + '", "' + results.data[a][4] + '");');
+                        obj.setAttribute('onclick', 'julkalenderModal("' + results.data[a][2] + '", "' + results.data[a][1] + '", "' + results.data[a][3] + '", "' + results.data[a][4] + '", "' + results.data[a][0] + '");');
                         if(getParameterByName('lucka') == results.data[a][0]){
-                            julkalenderModal(results.data[a][2], results.data[a][1], results.data[a][3], results.data[a][4]);
+                            julkalenderModal(results.data[a][2], results.data[a][1], results.data[a][3], results.data[a][4], results.data[a][0]);
                         };
                 };
             };
         };
     }
 });
-function julkalenderModal(text, datum, ref, href){
+function julkalenderModal(text, datum, ref, href, num){
     document.getElementById('kalender-datum').innerText = datum;
     document.getElementById('kalender-message').innerText = text;
+    var social = ['linkedin', 'facebook', 'twitter'];
+    for (let i = 0; i < social.length; i++) {
+        var element = document.getElementById(social[i] + '-modal');
+        element.setAttribute('href', element.getAttribute('href').split('?lucka')[0] + '?lucka=' + num);
+    };
     var objRef = document.getElementById('kalender-reference');
         removeElements(objRef);
         if(!href || href == '' ){
