fba9967bc167140052be92e692c57f0cb5f6f68d
Mattias Måsbäck <teddykladdkak@gmail.com>
2022-01-09 22:36:33 +0100

    Kontakt

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,25 +1,37 @@
-
+---
+sitemap:
+  exclude: 'yes'
+---
+{%- if site.url == "http://0.0.0.0:4000" -%}{%- capture postlink -%}{{ site.microserver.kontakt.local }}{%- endcapture -%}{%- else -%}{%- capture postlink -%}{{ site.microserver.kontakt.live }}{%- endcapture -%}{%- endif -%}
+function handlePostEvent(e) {
+    console.log(`Kunde inte skicka meddelande pga: "${e.type}" (${e.loaded} bytes transferred)`);
+};
 var postXhr = new XMLHttpRequest();
+    postXhr.addEventListener('error', handlePostEvent);
 var windowTextID = '';
     postXhr.onreadystatechange = function () {
+        console.log("readyState = " + this.readyState);
         if (this.readyState != 4) return;
         document.getElementById(windowTextID).value = '';
+        console.log("status = " + this.status);
         if (this.status == 200) {
-            var wrapper = document.getElementById('contact-form');
-            if(this.responseText == 'ok'){
-                oppnaModal('sendSuccess');
-            }else{
-                oppnaModal('sendError');
-            };
+            console.log(this.responseText);
+            oppnaModal('sendSuccess');
         }else{
             oppnaModal('sendError');
         };
+        document.getElementById('contactButton').removeAttribute('disabled');
     };
-function post(el, textID, url){
-    el.setAttribute('disabled', 'disabled');
+function post(el, textID){
     window['windowTextID'] = textID;
     var text = document.getElementById(textID).value;
-    postXhr.open('POST', 'https://hooks.slack.com/services/' + url, true);
-    postXhr.send(JSON.stringify({"text": text}));
+    if(!text || text == ''){
+        oppnaModal('sendError');
+    }else{
+        el.setAttribute('disabled', 'disabled');
+        postXhr.open('POST', '{{ postlink }}', true);
+        postXhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        postXhr.send('meddelande=' + text);
+    };
 };
 function checkEnter(event, buttonID){ if (event.keyCode === 13) { document.getElementById(buttonID).click(); }; };
